d6d024cb5ce0066893003b2a7bc39c5cc80e07ee
stan-holberton <9857@holbertonstudents.com>
2024-12-09 22:24:02 +0000

    Ajout de la fonction binary_tree_is_leaf pour vérifier si un nœud est une feuille

--- binary_trees.h
+++ binary_trees.h
@@ -28,4 +28,6 @@ void binary_tree_print(const binary_tree_t *tree);
 
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 
+int binary_tree_is_leaf(const binary_tree_t *node);
+
 #endif /* BINARY_TREES_H */
