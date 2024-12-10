97dd4eaf8b8e584177fefd5dcdf40bcde560e899
stan-holberton <9857@holbertonstudents.com>
2024-12-10 14:45:58 +0000

    Mise a jout du fichier d'en-tete

--- binary_trees.h
+++ binary_trees.h
@@ -34,4 +34,8 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 
+size_t binary_tree_depth(const binary_tree_t *tree);
+
+size_t binary_tree_leaves(const binary_tree_t *tree);
+
 #endif /* BINARY_TREES_H */
