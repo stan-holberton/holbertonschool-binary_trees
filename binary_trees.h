80bac62b119e8ff5b0393f20b729953fcf189ce0
stan-holberton <9857@holbertonstudents.com>
2024-12-12 18:30:07 +0000

    mise a jour du fichier .h pour ajouter la fonction pour la task 3

--- binary_trees.h
+++ binary_trees.h
@@ -44,4 +44,6 @@ size_t binary_tree_height(const binary_tree_t *tree);
 
 int binary_tree_balance(const binary_tree_t *tree);
 
+void binary_tree_delete(binary_tree_t *tree);
+
 #endif /* BINARY_TREES_H */
