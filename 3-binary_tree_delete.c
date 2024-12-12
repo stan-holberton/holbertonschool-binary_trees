2c680a6dbc63067081a90f4e8dd8135ba3b2649e
stan-holberton <9857@holbertonstudents.com>
2024-12-12 18:26:57 +0000

    Ajout de la fonction binary_tree_delete pour supprimer un arbre binaire

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,18 +1,22 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "binary_trees.h"
+#include <stdlib.h>
 
 /**
- * binary_tree_delete - deletes an entire binary tree
- * @tree: pointer to the root node of the tree to delete
+ * binary_tree_delete - Supprime un arbre binaire
+ * @tree: Pointeur vers le nœud racine de l'arbre à supprimer
+ *
+ * Description: La fonction supprime récursivement chaque nœud de l'arbre
+ * en commençant par les feuilles.
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
-    if (tree != NULL)
-        return;
-        
-                binary_tree_delete(tree->left);
-                binary_tree_delete(tree->right);
-                free(tree);
-        
-}        
+	if (tree == NULL)
+		return;
+
+	/* Supprimer les sous-arbres gauche et droit de manière récursive */
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+
+	/* Libérer la mémoire du nœud actuel */
+	free(tree);
+}
